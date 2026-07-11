113961aefdd0c97df2939b09fbca40b3bbf35115
dthwwydfli <abdullahistiaq77@gmail.com>
2026-07-11 13:48:58 +0100

    test: update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:accountability_app/main.dart';
 
 void main() {
-  testWidgets('App loads onboarding', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProviderScope(child: AccountabilityApp()));
-    await tester.pumpAndSettle();
-    expect(find.text('Stay accountable together'), findsOneWidget);
+  testWidgets('App builds without error', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: LavenderApp()));
+    await tester.pump();
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(tester.takeException(), isNull);
   });
 }
