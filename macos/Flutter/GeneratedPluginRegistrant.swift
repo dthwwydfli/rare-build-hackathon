19d684245f64c0602b8e3730553b11f5f06128a2
dthwwydfli <abdullahistiaq77@gmail.com>
2026-07-11 16:28:27 +0100

    chore: update macOS generated plugin registrant

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,11 +12,8 @@ import firebase_messaging
 import geolocator_apple
 import google_sign_in_ios
 import shared_preferences_foundation
-<<<<<<< HEAD
 import sign_in_with_apple
-=======
 import url_launcher_macos
->>>>>>> ba2564f (feat: block access and money feature)
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -26,9 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-<<<<<<< HEAD
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
-=======
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
->>>>>>> ba2564f (feat: block access and money feature)
 }
